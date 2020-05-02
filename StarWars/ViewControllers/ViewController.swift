5f379046b73475841263ee84e5c5b9513a69ff80
Renu Punjabi <rpunjabi12@gmail.com>
2020-05-02 05:27:44 -0700

    initial commit

---
+++ StarWars/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Renu Punjabi on 5/2/20.
+//  Copyright © 2020 Renu Punjabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
